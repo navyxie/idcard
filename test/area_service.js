967e41924571b6217f3013f799bde69a78e671c8
navyxie <navyxie2010@gmail.com>
2017-07-27 12:28:28 +0800

    新增15位身份证升级到18位身份证

--- test/area_service.js
+++ test/area_service.js
@@ -1,56 +1,80 @@
 var should = require('should');
 var area_service = require('../lib/area_service');
-describe('area_service',function(){
-	describe('#hit()',function(){
-		it('should be ok',function(){
-			area_service.hit('440882199102104195');
-		});
-		it('should not be ok',function(){
-			area_service.hit('40002003010202260X');
-		});
-		it('should not be ok',function(){
-			area_service.hit('');
-		});		
-	});
-	describe('#idcardinfo()',function(){
-		it('should be ok',function(){
-			area_service.idcardinfo('440882199102104195').should.have.properties({'birthday':19910210,gender:'M',address:"广东省湛江市雷州市"});
-		});
-		it('should be ok',function(){
-			area_service.idcardinfo('440711198806181519').should.have.properties({'birthday':19880618,gender:'M',address:"广东省江门市郊区"});
-		});	
-		it('should be ok',function(){
-			area_service.idcardinfo('441900198410125825').should.have.properties({'birthday':19841012,gender:'F',address:"广东省东莞市"});
-		});
-		it('should be ok',function(){
-			area_service.idcardinfo('150301197707291508').should.have.properties({'birthday':19770729,gender:'F',address:"内蒙古自治区乌海市市辖区"});
-		});	
-		it('should be ok',function(){
-			area_service.idcardinfo('65282419840210443X').should.have.properties({'birthday':19840210,gender:'M',address:"新疆维吾尔自治区巴音郭楞蒙古自治州若羌县"});
-		});	
-		it('should not be ok',function(){
-			area_service.idcardinfo('40002003010202260X').should.have.properties({'valid':false});
-		});
-		it('should not be ok',function(){
-			area_service.idcardinfo('').should.have.properties({'valid':false});
-		});		
-	});
-	describe('#getAge()',function(){
-		it('should be ok',function(){
-			area_service.getAge(19910210).should.be.aboveOrEqual(25);
-		});
-		it('should be ok',function(){
-			should(area_service.getAge()).undefined();
-		});
-		it('should be ok',function(){
-			var curDate = new Date();
-			var preTwoYearAge = curDate.getTime() - (366+1)*24*60*60*1000;
-			curDate = new Date(preTwoYearAge);
-			var year = curDate.getFullYear();
-			var month = 1+ curDate.getMonth();
-			var date = curDate.getDate();
-			var tempBirthday = year+''+(month < 10 ? '0' + month : month) + '' + date;
-			area_service.getAge(tempBirthday).should.be.equal(1);
-		});
-	})
+describe('area_service', function() {
+  describe('#hit()', function() {
+    it('should be ok', function() {
+      area_service.hit('440882199102104195');
+    });
+    it('should not be ok', function() {
+      area_service.hit('40002003010202260X');
+    });
+    it('should not be ok', function() {
+      area_service.hit('');
+    });
+  });
+  describe('#idcardinfo()', function() {
+    it('should be ok', function() {
+      area_service.idcardinfo('440882199102104195').should.have.properties({
+        'birthday': 19910210,
+        gender: 'M',
+        address: "广东省湛江市雷州市"
+      });
+    });
+    it('should be ok', function() {
+      area_service.idcardinfo('440711198806181519').should.have.properties({
+        'birthday': 19880618,
+        gender: 'M',
+        address: "广东省江门市郊区"
+      });
+    });
+    it('should be ok', function() {
+      area_service.idcardinfo('441900198410125825').should.have.properties({
+        'birthday': 19841012,
+        gender: 'F',
+        address: "广东省东莞市"
+      });
+    });
+    it('should be ok', function() {
+      area_service.idcardinfo('150301197707291508').should.have.properties({
+        'birthday': 19770729,
+        gender: 'F',
+        address: "内蒙古自治区乌海市市辖区"
+      });
+    });
+    it('should be ok', function() {
+      area_service.idcardinfo('65282419840210443X').should.have.properties({
+        'birthday': 19840210,
+        gender: 'M',
+        address: "新疆维吾尔自治区巴音郭楞蒙古自治州若羌县"
+      });
+    });
+    it('should not be ok', function() {
+      area_service.idcardinfo('40002003010202260X').should.have.properties({
+        'valid': false
+      });
+    });
+    it('should not be ok', function() {
+      area_service.idcardinfo('').should.have.properties({
+        'valid': false
+      });
+    });
+  });
+  describe('#getAge()', function() {
+    it('should be ok', function() {
+      area_service.getAge(19910210).should.be.aboveOrEqual(25);
+    });
+    it('should be ok', function() {
+      should(area_service.getAge()).undefined();
+    });
+    it('should be ok', function() {
+      var curDate = new Date();
+      var preTwoYearAge = curDate.getTime() - (366 + 1) * 24 * 60 * 60 * 1000;
+      curDate = new Date(preTwoYearAge);
+      var year = curDate.getFullYear();
+      var month = 1 + curDate.getMonth();
+      var date = curDate.getDate();
+      var tempBirthday = year + '' + (month < 10 ? '0' + month : month) + '' + date;
+      area_service.getAge(tempBirthday).should.be.equal(1);
+    });
+  })
 })
--- test/util.js
+++ test/util.js
@@ -1,56 +1,61 @@
 var should = require('should');
 var util = require('../lib/util');
-describe('#util#',function(){
-	describe('verifyBirthday()',function(){
-		it('it must be no ok',function(){
-			util.verifyBirthday('19910229').should.be.false();
-		});
-		it('it must be no ok',function(){
-			util.verifyBirthday('29910229').should.be.false();
-		});
-		it('it must be no ok',function(){
-			util.verifyBirthday('19910431').should.be.false();
-		});
-		it('it must be no ok',function(){
-			util.verifyBirthday('19910431').should.be.false();
-		});
-		it('it must be ok',function(){
-			util.verifyBirthday('19910210').should.be.true();
-		});
-		it('it must be ok',function(){
-			util.verifyBirthday('20000229').should.be.true();
-		});
-		it('it must be ok',function(){
-			util.verifyBirthday('19910331').should.be.true();
-		});
-		it('it must be ok',function(){
-			util.verifyBirthday('19910430').should.be.true();
-		});
-	});
-	describe('util',function(){
-		it('it must be ok',function(){
-			util.isObject({}).should.be.true();
-		});
-		it('it must be ok',function(){
-			util.isString({}).should.be.false();
-		});
-		it('it must be ok',function(){
-			util.isFunction({}).should.be.false();
-		});
-		it('it must be ok',function(){
-			util.isNumber({}).should.be.false();
-		});
-		it('it must be ok',function(){
-			util.getDefaultIdcardInfoReturn().should.properties({valid:false});
-		});
-		it('it must be ok',function(){
-			util.keys({a:1,b:2}).should.containDeep(['a','b']);
-			util.keys([1,2]).should.be.empty();
-		});
-		it('it must be ok',function(){
-			should(util.randomNum()).undefined();
-			util.randomNum(4,5).should.be.aboveOrEqual(4);
-			util.randomNum(4,5).should.be.belowOrEqual(5);
-		});
-	})
+describe('#util#', function() {
+  describe('verifyBirthday()', function() {
+    it('it must be no ok', function() {
+      util.verifyBirthday('19910229').should.be.false();
+    });
+    it('it must be no ok', function() {
+      util.verifyBirthday('29910229').should.be.false();
+    });
+    it('it must be no ok', function() {
+      util.verifyBirthday('19910431').should.be.false();
+    });
+    it('it must be no ok', function() {
+      util.verifyBirthday('19910431').should.be.false();
+    });
+    it('it must be ok', function() {
+      util.verifyBirthday('19910210').should.be.true();
+    });
+    it('it must be ok', function() {
+      util.verifyBirthday('20000229').should.be.true();
+    });
+    it('it must be ok', function() {
+      util.verifyBirthday('19910331').should.be.true();
+    });
+    it('it must be ok', function() {
+      util.verifyBirthday('19910430').should.be.true();
+    });
+  });
+  describe('util', function() {
+    it('it must be ok', function() {
+      util.isObject({}).should.be.true();
+    });
+    it('it must be ok', function() {
+      util.isString({}).should.be.false();
+    });
+    it('it must be ok', function() {
+      util.isFunction({}).should.be.false();
+    });
+    it('it must be ok', function() {
+      util.isNumber({}).should.be.false();
+    });
+    it('it must be ok', function() {
+      util.getDefaultIdcardInfoReturn().should.properties({
+        valid: false
+      });
+    });
+    it('it must be ok', function() {
+      util.keys({
+        a: 1,
+        b: 2
+      }).should.containDeep(['a', 'b']);
+      util.keys([1, 2]).should.be.empty();
+    });
+    it('it must be ok', function() {
+      should(util.randomNum()).undefined();
+      util.randomNum(4, 5).should.be.aboveOrEqual(4);
+      util.randomNum(4, 5).should.be.belowOrEqual(5);
+    });
+  })
 })
